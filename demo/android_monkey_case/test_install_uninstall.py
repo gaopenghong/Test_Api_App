cb928e2060121207fea1c6a3d12e54b2147dc557
gaopenghong <gphisf@126.com>
2020-06-11 16:03:41 +0800

    上传代码

---
+++ demo/android_monkey_case/test_install_uninstall.py
@@ -0,0 +1,41 @@
+import os
+import time
+import unittest
+
+from common.appium_common.read_config import read_package_name, read_activity_name
+from common.appium_common.shell_boot_adb import shell
+from utx import log
+
+
+class TestInstall(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.app = '牛老幺'
+        cls.package = read_package_name(cls.app)
+        cls.activity = read_activity_name(cls.app)
+        cls.apk_path = r"D:\PythonWorkSpace\Test_Api_App\data\app-release.apk"
+
+    def test_install(self):
+        adb = "adb install  %s" % self.apk_path
+        print(adb)
+        r = os.system(adb)
+        log.info("安装成功")
+        return r
+
+    def test_uninstall(self):
+        time.sleep(5)
+        adb = "adb   uninstall  %s" % self.package
+        r = os.system(adb)
+        log.info("安装成功")
+        return r
+
+    def test_reinstall(self):
+        adb = "adb install  %s" % self.apk_path
+        print(adb)
+        r = os.system(adb)
+        log.info("再次安装成功")
+        return r
+
+
+if __name__ == '__main__':
+    unittest.main()
