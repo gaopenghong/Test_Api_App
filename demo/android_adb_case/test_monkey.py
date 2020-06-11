4baacd61b1f95b0641098e9e412ab9e9eafe56ca
gaopenghong <gphisf@126.com>
2020-06-11 18:01:32 +0800

    上传代码

--- demo/android_adb_case/test_monkey.py
+++ demo/android_adb_case/test_monkey.py
@@ -13,7 +13,7 @@ class MonkeyCase(unittest.TestCase):
         cls.app = '牛老幺'
         cls.package = read_package_name(cls.app)
         cls.activity = read_activity_name(cls.app)
-        cls.path = r"D:\\PythonWorkSpace\\Test_Api_App\\demo\\report"
+        cls.path = "D:\\PythonWorkSpace\\Test_Api_App\\demo\\report"
 
     def test_monkey(self):
         monkey_seed = str(random.randrange(1, 1000))
@@ -23,11 +23,11 @@ class MonkeyCase(unittest.TestCase):
         deviceId = re.findall(r'^\w*\b', readDeviceId[1])[0]
         monkey_parameters = " --ignore-timeouts --pct-touch 40 --pct-trackball 40 --pct-appswitch 0  --pct-motion 20 -v -v  --throttle  200 100"
         shell_monkey = "shell monkey -p %s -s %s %s" % (self.package, monkey_seed, monkey_parameters)
-        monkey_log = self.path + r"\\log\\" + "monkey.log"
-        adb_monkey = "adb  -s %s %s > %s" % (deviceId, shell_monkey, str(monkey_log))
-        # os.popen(adb_monkey)
+        monkey_log = self.path + "\\monkey.log"
+        adb_monkey = "adb  -s %s %s>%s" % (deviceId, shell_monkey,monkey_log)
+        os.system(adb_monkey)
 
-        print(adb_monkey)
+        print(monkey_log)
 
 
 if __name__ == '__main__':
