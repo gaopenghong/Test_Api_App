eb3930c301b626959d62271403ced56565b686a2
gaopenghong <gphisf@126.com>
2020-06-12 13:09:56 +0800

    上传代码

--- common/app_common/shell_install_adb.py
+++ common/app_common/shell_install_adb.py
@@ -2,8 +2,8 @@ import os
 import time
 
 
-class InstallUninstall:
-    def install_status(self,package):
+class InstallUninstall():
+    def apk_install_status(self,package):
         """
         功能查找是否已经安装此安装包
         package：包名
@@ -16,18 +16,17 @@ class InstallUninstall:
         else:
             return False
 
-    def install(self,apk_path):
+    def apk_install(self,apk_path):
         """
         功能安装APP
         package：包名
         apk_path：包存放路径
-
         """
         adb = "adb install  %s" % apk_path
         print(adb)
         os.system(adb)
 
-    def uninstall(self,package):
+    def apk_uninstall(self,package):
         """
        功能： 卸载APP
         package：包名
--- demo/android_adb_case/test.py
+++ demo/android_adb_case/test.py
@@ -1,11 +1,11 @@
-import os
 import time
 import unittest
 from common.app_common.read_config import *
+from common.app_common.shell_install_adb import InstallUninstall
 from utx import *
 
 
-class TestInstall(unittest.TestCase):
+class TestInstallUninstall(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.app = '牛老幺'
@@ -15,42 +15,30 @@ class TestInstall(unittest.TestCase):
     @tag(Tag.INSTALL)
     def test_install_status(self):
         """查找是否已经安装此安装包,如果有的话删除旧包，如果没有安装就直接安装"""
-        adb_monkey2 = """adb  shell  pm list packages | find "%s" """ % self.package # adb命令查看是否已经安装此包
-        status = os.popen(adb_monkey2).read()
-        log.info(status)
-        if status:
-            adb = "adb   uninstall  %s" % self.package
-            os.system(adb)
-            log.info('开始卸载旧包')
+        s = InstallUninstall().apk_install_status(self.package)
+        if s:
+            InstallUninstall().apk_uninstall(self)
         else:
             log.info("未安装旧包，可以直接安装新包哈哈")
 
-    @tag(Tag.INSTALL,Tag.SMOKE)
+    @tag(Tag.INSTALL, Tag.SMOKE)
     def test_install(self):
         """安装APP"""
-        adb = "adb install  %s" % self.apk_path
-        print(adb)
-        r = os.system(adb)
+        InstallUninstall().apk_install(self.apk_path)
         log.info("安装成功")
-        return r
 
     @tag(Tag.INSTALL)
     def test_uninstall(self):
         """卸载APP"""
         time.sleep(5)
-        adb = "adb   uninstall  %s" % self.package
-        r = os.system(adb)
+        InstallUninstall().apk_uninstall(self.package)
         log.info("卸载成功")
-        return r
 
     @tag(Tag.INSTALL)
     def test_reinstall(self):
         """再次安装APP"""
-        adb = "adb install  %s" % self.apk_path
-        print(adb)
-        r = os.system(adb)
+        InstallUninstall().apk_install(self.apk_path)
         log.info("再次安装成功")
-        return r
 
 
 if __name__ == '__main__':
