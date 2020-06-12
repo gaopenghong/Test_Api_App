fee07163ed67b8e282e8eef13a4ef89febf0a828
gaopenghong <gphisf@126.com>
2020-06-12 13:38:40 +0800

    上传代码

--- demo/android_adb_case/test_3_monkey.py
+++ demo/android_adb_case/test_3_monkey.py
@@ -1,7 +1,10 @@
+import time
 import unittest
 import warnings
+from common.app_common.element_action import *
 from common.app_common.read_config import *
-from common.app_common.shell_monkey_adb import MonkeyShell
+from common.app_common.session import *
+from common.app_common.shell_monkey_adb import *
 from utx import *
 
 
@@ -13,12 +16,37 @@ class MonkeyCase(unittest.TestCase):
         cls.package = read_package_name(cls.app)  # 包名获取
         cls.path = "D:\\PythonWorkSpace\\Test_Api_App\\demo\\report"  # monkey日志存放路径
         cls.times = int(100)  # monkey执行时间次数
+        cls.user_number = 18636299591
+        cls.password = 123456
+        cls.driver = driver_begin(cls.app)
+        log.debug("初始化APP，测试数据初始化")
+        time.sleep(1)
+        log.info("开始执行登录操作")
+        log.info("----" * 15)
+        inputting(driver=cls.driver, elements_name=2, section_name='登录页面', name='输入框', txt=cls.user_number)
+        log.info("输入账号")
+        inputting(driver=cls.driver, elements_name=2, section_name='登录页面', name='密码', txt=cls.password)
+        log.info("输入密码")
+        clicking(driver=cls.driver, elements_name=2, section_name='登录页面', name='登录按钮')
+        log.info("点击登录")
+        time.sleep(2)
+        log.info("操作权限弹窗三次")
+        for i in range(3):
+            clicking(driver=cls.driver, elements_name=2, section_name='权限', name='允许')
+            time.sleep(1.5)
+
 
     @tag(Tag.MONKEY)
-    def test_monkey(self):
+    def test_monkey_fast(self):
         """执行monkey测试，导出日志"""
         MonkeyShell(self.package, self.times, self.path).monkey()
-        log.info("开始执行monkey脚本，大约用时%s分钟"% str(self.times / 120))
+        log.info("开始执行monkey脚本，大约用时%s分钟" % str(self.times / 120))
+
+    @tag(Tag.MONKEY)
+    def test_monkey_slow(self):
+        """执行monkey测试，导出日志"""
+        MonkeyShell(self.package, self.times, self.path).monkey()
+        log.info("开始执行monkey脚本，大约用时%s分钟" % str(self.times / 120))
 
 
 if __name__ == '__main__':
