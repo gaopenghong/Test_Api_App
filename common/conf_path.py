121211a34264bfb4b4d98926adb9db3f9e42c8bf
gaopenghong <gphisf@126.com>
2020-06-16 09:22:55 +0800

    上传代码

--- common/conf_path.py
+++ common/conf_path.py
@@ -1,3 +1,4 @@
+# 配置文件的绝对路径
 app_conf_path = r"D:\PythonWorkSpace\Test_Api_App\config\app.ini"
 ele_class_name_conf_path = "D:\PythonWorkSpace\Test_Api_App\config\ele_class_name.ini"
 ele_id_conf_path = "D:\PythonWorkSpace\Test_Api_App\config\ele_id.ini"
