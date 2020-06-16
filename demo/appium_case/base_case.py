fa9e95b8439edb8445e81666e46bc3cad5855391
gaopenghong <gphisf@126.com>
2020-06-16 10:51:57 +0800

    上传代码

--- demo/appium_case/base_case.py
+++ demo/appium_case/base_case.py
@@ -1,3 +1,4 @@
+from demo.base_page_class.base_driver import *
 from demo.base_page_class.base_login_pageUI import *
 from demo.base_page_class.base_mine_pageUI import *
 from demo.base_page_class.base_message_pageUI import *
@@ -5,11 +6,11 @@ from demo.base_page_class.base_message_pageUI import *
 class BaseCaseSetup(unittest.TestCase):
     @classmethod
     def setUpClass(cls):  # setUpClass所有用例开始前执行一遍，但是必须使用类函数装饰器
-        cls.driver = BasePage().driver_begin(app_name_nly)
+        cls.driver = BaseDriver().driver_begin()
         log.debug("初始化APP，测试数据初始化")
-        BasePage().login_base(driver=cls.driver, user_number=user_1, password=password)
+        BasePageLogin().login_base(driver=cls.driver, user_number=user_1, password=password)
         for i in range(3):
-            BasePage().skip_limits(cls.driver)
+            BasePageLogin().skip_limits(cls.driver)
 
 
 if __name__ == '__main__':
---
+++ demo/base_page_class/base_driver.py
@@ -0,0 +1,27 @@
+from demo.my_test import *
+
+class BaseDriver():
+
+    def driver_begin(self):
+        """
+        driver驱动启动
+        """
+        deviceName = os.popen('adb shell getprop ro.product.model').read()  # 获取设备名称
+        log.info("测试设备为%s" % deviceName)
+        # 读取设备系统版本号
+        deviceAndroidVersion = list(os.popen('adb shell getprop ro.build.version.release').readlines())
+        log.info("测试设备系统版本为%s" % deviceAndroidVersion)
+        deviceVersion = re.findall(r'^\w*\b', deviceAndroidVersion[0])[0]
+        # package_name = read_ini(ini_file_path=app_conf_path, name=app, value='appPackage')
+        platformName = 'Android'
+        desired_caps = {
+            'platformName': platformName,
+            'deviceName': deviceName,
+            'platformVersion': deviceVersion,
+            'appPackage': package_name_nly,
+            'appActivity': activity_name_nly,
+            'noReset': False,
+            'resetKeyboard': False  # 将键盘给隐藏起来
+            # 'unicodeKeyboard': True,# 使用unicodeKeyboard的编码方式来发送字符串
+        }
+        return webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
