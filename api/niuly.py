8cd554528944f713fe6a8c6fb3098140c9ca126f
gaopenghong <gphisf@126.com>
2020-06-30 17:47:25 +0800

    上传代码

--- api/niuly.py
+++ api/niuly.py
@@ -16,7 +16,6 @@ def get_room():
     re = requests.get(url=url, params=params)
     print(re.json())
 
-def ddd():
-    url = "http://121.89.182.48:8094/room/get?pageSize=50&roomId=5edb59def058f456c2b3dae5&language=zh&access_token=56bf7ef035f94e96b13849af8457ae57&salt=1591696330818&secret=uvbQEhie0qifsuZ9NZRaWw%3D%3D"
+
 if __name__ == '__main__':
     get_room()
