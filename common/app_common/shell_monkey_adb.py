958f9350beb8ed91bdbae32b70a6755445922418
gaopenghong <gphisf@126.com>
2020-07-03 11:52:02 +0800

    上传代码

--- common/app_common/shell_monkey_adb.py
+++ common/app_common/shell_monkey_adb.py
@@ -17,7 +17,7 @@ class MonkeyShell():
         # 正则表达式匹配出 id 信息
         deviceId = re.findall(r'^\w*\b', readDeviceId[1])[0]
         # monkey参数设置
-        monkey_parameters = " --ignore-timeouts --pct-touch 40 --pct-trackball 40 --pct-appswitch 0  --pct-motion 20 -v -v  --throttle  200 %s" % self.times
+        monkey_parameters = " --ignore-timeouts --pct-touch 40 --pct-trackball 40 --pct-appswitch 0  --pct-motion 20 -v -v -v --throttle  200 %s" % self.times
         # monkey脚本拼接
         shell_monkey = "shell monkey -p %s -s %s %s" % (self.package, monkey_seed, monkey_parameters)
         # 日志输出命令
